89cad8e0dc703f3f9a88e90f307afc57b9635576
Ben Klassen <benklassen@Bens-MacBook-Pro.local>
2024-12-07 21:15:07 -0500

    adding other dashboards

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 console.log('Is this working?');
 
 let viz;
-
-//Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/views/Patent_Pulse/EconomicStatistics?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link";
-
 const vizContainer = document.getElementById('vizContainer');
 const options = {
     hideTabs: true,
@@ -17,33 +13,33 @@ const options = {
     }
 };
 
+//Add Share Link to Tableau Public in here
+let url = "https://public.tableau.com/views/Patent_Pulse/PatentStatistics?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link";
+
 //create a function to generate the viz element
 function initViz() {
     console.log('Executing the initViz function!');
+    if (viz) {
+        viz.dispose(); // Remove existing viz
+    }
     viz = new tableau.Viz(vizContainer, url, options);
 }
 
 // run the initViz function when the page loads
 document.addEventListener("DOMContentLoaded", initViz);
 
-const exportPDF = document.getElementById('exportPDF');
-const exportImage = document.getElementById('exportImage');
+// Add event listeners for dashboard switching buttons
+document.getElementById('dashboard1').addEventListener('click', function() {
+    url = "https://public.tableau.com/views/Patent_Pulse/PatentStatistics?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link";
+    initViz();
+});
 
+document.getElementById('dashboard2').addEventListener('click', function() {
+    url = "https://public.tableau.com/views/Patent_Pulse/EconomicStatistics?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link";
+    initViz();
+});
 
-//click on the pdf button to generate pdf of dashboard
-function generatePDF() {
-    viz.showExportPDFDialog()
-}
-
-exportPDF.addEventListener("click", function () {
-    generatePDF();
-  });
-
-//click on image to generate image of dashboard
-function generateImage() {
-    viz.showExportImageDialog()
-}
-
-exportImage.addEventListener("click", function () {
-    generateImage();
-  });
+document.getElementById('dashboard3').addEventListener('click', function() {
+    url = "https://public.tableau.com/views/Patent_Pulse/InnovationScores?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link";
+    initViz();
+});
